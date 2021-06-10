98515c7c9dd10c471228a0f67b392675d973cd92
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-10 14:24:32 +0300

    Update App.js
    lesson 17

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const element = (<div> Hello World! </div>);
+const ItemList = () => (<ul>
+  <li>1</li>
+  <li>2</li>
+  </ul>);
 
-export {element};
+const App = () => (<div>
+  <h1>Hello World!</h1>
+  <ItemList /> 
+  </div>);
+
+export default App;
