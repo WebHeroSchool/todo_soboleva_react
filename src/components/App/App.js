c47c597c6b5d020f6eb97d2523d8b7f3c91f8649
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-11 01:59:58 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
+import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
+
+const todoItem = 'Написать новое приложение';
 
 const App = () => (
   <div>
-   <h1> Важные дела </h1>
-   <ItemList />
+   <h1> Важные дела: </h1>
+   <InputItem />
+   <ItemList todoItem={todoItem} />
+   <Footer count={3} />
   </div>
 );
 
