b41f967162da2f6f959cb2d879515c8d604560d1
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-11 02:00:16 +0300

    Update ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Item from '../Item/Item';
 
-const ItemList = () => (
+const ItemList = ({todoItem}) => (
   <ul>
-    <li><Item /></li>
-    <li><Item /></li>
-    <li><Item /></li>
+    <li><Item todoItem={todoItem}/></li>
+    <li><Item todoItem={'Прописать props-ы'}/></li>
+    <li><Item todoItem={'Закончить задание'}/></li>
   </ul>
 );
 
