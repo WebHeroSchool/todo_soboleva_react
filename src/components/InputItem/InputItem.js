1056b0dae4e828613abe5f66c984a443a10d3ef4
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-16 01:46:53 +0300

    Update InputItem.js

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<div>InputItem</div>);
+const InputItem = () => (<div>
+  <TextField
+          id="standard-read-only-input"
+          label="Записать новое задание"
+          defaultValue=""
+          InputProps={{
+            readOnly: true,
+          }}
+  />
+  </div>);
 
 export default InputItem;
