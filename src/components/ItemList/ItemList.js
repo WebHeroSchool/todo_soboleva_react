2e687ff29c9ca70a23e722412faaf1ada3d90999
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-18 01:54:53 +0300

    Update ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import List from '@material-ui/core/List';
 import Item from '../Item/Item';
 
-const ItemList = ({ items }) => (<ul>
-     {items.map(item => <li key={item.value}>
-          <Item value={item.value} isDone={item.isDone} />
-     </li>)}
-</ul>);
+const ItemList = ({ items, onClickDone, onClickDelete }) => (<List>
+  {items.map(item => <li key={item.id}>
+     <Item 
+       value={item.value} 
+       isDone={item.isDone} 
+       id={item.id} 
+       onClickDone={onClickDone} 
+       onClickDelete={onClickDelete}
+       />
+  </li>)}
+</List>);
+
+ItemList.defaultProps = {
+  value: 'add task',
+  isDone: false,
+  id: 0
+};
+
 
 export default ItemList;
