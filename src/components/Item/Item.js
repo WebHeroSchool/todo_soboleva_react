b3198378423fdcde9c14a9c04d833982dcb0d85d
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-18 01:54:45 +0300

    Update Item.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import classnames from 'classnames';
-import styles from './item.module.css';
+import styles from './Item.module.css';
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-const Item = ({value, isDone}) => (<span className={
-  classnames({
-    [styles.item]:true,
-    [styles.done]:isDone
-  })
-}>
-    {value}
-</span>);
+
+const Item = ({ value, isDone, onClickDone, onClickDelete, id }) => (
+    <div className={styles.wrap}>
+		<Checkbox
+            checked={isDone}
+	        color="default"
+	        inputProps={{ 'aria-label': 'primary checkbox' }}
+            onClick={() => onClickDone(id)}
+	      />
+		<label className = {
+			classnames({
+				[styles.item]: true,
+				[styles.done]: isDone
+			})
+		}> {value}
+		</label>
+             <DeleteIcon className={styles.btn} fontSize="large"
+             onClick={() => onClickDelete(id)} />
+       </div>);
 
 export default Item;
