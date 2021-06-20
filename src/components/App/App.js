92fa03ab57d839947da7f7123bc02f2675fb0a26
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-21 00:23:18 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,71 +4,28 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-class App extends React.Component {
-    state = {
-        items: [
+const App = () => {
+  const items = [
             {
                 value: 'Написать новое приложение',
                 isDone: true,
-                id: 1
             },
             {
                 value: 'Прописать props',
                 isDone: false,
-                id: 2
             },
             {
                 value: 'Сделать все дела',
                 isDone: true,
-                id: 3
             }
-        ],
-        count: 3
-    };
-
-onClickDone = id => {
-    const newItemList = this.state.items.map(item => {
-        const newItem = { ...item};
-
-        if (item.id === id) {
-            newItem.isDone = !item.isDone;
-        }
-
-        return newItem;
-    });
-
-    this.setState({ items: newItemList});
-};
-
-onClickDelete = id => this.setState(state => ({ items: state.items.filter(item => item.id !== id)}));
-
-onClickAdd = value => this.setState(state => ({
-            items:[
-             ...state.items,
-             {
-                  value,
-                  isDone: false,
-                    id: state.count + 1
-             }
-            ],
-        count: state.count + 1,
-    }));
-
-
-  render () {
+      ];
         return (
-        <div className={styles.wrap}>
-                <h1 className={styles.title}>Важные дела:</h1>
-                <InputItem
-                onClickAdd={this.onClickAdd}
-                />
-                <ItemList
-                   items={this.state.items}
-                   onClickDone={this.onClickDone}
-                   onClickDelete={this.onClickDelete} />
-                <Footer count={this.state.count} />
-        </div>);
-    }
-};
+          <div className={styles.wrap}>
+          <h1 className={styles.title}>Важные дела:</h1>
+          <InputItem />
+          <ItemList items={items} />
+          <Footer count={3}/>
+      </div>);
+}
 
 export default App;
