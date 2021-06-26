94326b2643957f7e28660dfe879317371b1cc760
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-27 02:04:57 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,23 +5,20 @@ import Button from '@material-ui/core/Button';
 const Footer = ({ count }) => (<footer>
 <div className={styles.footer}>Осталось выполнить дел: {count}</div>
 <div className={styles.button}>
-    <Button href="#text-buttons" color="#d3d3d3" style={{ fontSize: '8px'}}>
+    <Button href="#text-buttons" color="#d3d3d3">
     All
     </Button>
-    <Button href="#text-buttons" color="#d3d3d3" style={{ fontSize: '8px'}}>
+    <Button href="#text-buttons" color="#d3d3d3">
     Active
     </Button>
-    <Button href="#text-buttons" color="#d3d3d3" style={{ fontSize: '8px'}}>
+    <Button href="#text-buttons" color="#d3d3d3">
     Completed
     </Button>
-    <Button href="#text-buttons" color="#d3d3d3" style={{ fontSize: '8px'}}>
+    <Button href="#text-buttons" color="#d3d3d3">
     Clear completed
     </Button>
 </div>
 </footer>)
 
-Footer.defaultProps = {
-    count: 0
-};
 
 export default Footer;
