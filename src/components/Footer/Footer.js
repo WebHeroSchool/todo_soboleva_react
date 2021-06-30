522e180b9dc3802b7201e44c759223668acbc8dd
Maria Soboleva <soboleva.mar89@gmail.com>
2021-06-30 16:07:34 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const Footer = ({ count }) => (<footer>
 <div className={styles.footer}>Осталось выполнить дел: {count}</div>
-<div className={styles.button}>
-    <Button href="#text-buttons" color="#d3d3d3">
-    All
-    </Button>
-    <Button href="#text-buttons" color="#d3d3d3">
-    Active
-    </Button>
-    <Button href="#text-buttons" color="#d3d3d3">
-    Completed
-    </Button>
-    <Button href="#text-buttons" color="#d3d3d3">
-    Clear completed
-    </Button>
-</div>
+<ButtonGroup variant="text" size="small" color="secondary" aria-label="text primary button group">
+  <Button>All</Button>
+  <Button>Active</Button>
+  <Button>Completed</Button>
+</ButtonGroup>
 </footer>)
 
 
