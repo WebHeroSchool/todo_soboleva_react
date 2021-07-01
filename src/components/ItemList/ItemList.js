99c5965f44d97e54ead84b6b4334dab4d4ce55bb
Maria Soboleva <soboleva.mar89@gmail.com>
2021-07-01 23:44:44 +0300

    Update ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,13 +3,14 @@ import List from '@material-ui/core/List';
 import styles from './ItemList.module.css';
 import Item from '../Item/Item';
 
-const ItemList = ({items, onClickDone}) => (<List>
+const ItemList = ({items, onClickDone, onClickDelete}) => (<List>
     {items.map(item => <li key={item.value}>
         <Item
           value={item.value}
           isDone={item.isDone}
           id={item.id}
-          onClickDone={onClickDone}/>
+          onClickDone={onClickDone}
+          onClickDelete={onClickDelete}/>
     </li>)}
 </List>);
 
