f73cfb96495135fb462cfedd2be41d4de28e8b00
Maria Soboleva <soboleva.mar89@gmail.com>
2021-07-05 00:49:34 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -64,7 +64,7 @@ class App extends React.Component {
              items={this.state.items}
              onClickDone={this.onClickDone}
              onClickDelete={this.onClickDelete}/>
-          <Footer/>
+          <Footer count={this.state.count}/>
       </div>);
    }
 };
