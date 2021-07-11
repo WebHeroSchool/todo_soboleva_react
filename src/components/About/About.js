1d0a49cb461f817bac2055637f6015b9c5460632
Maria Soboleva <soboleva.mar89@gmail.com>
2021-07-12 00:46:38 +0300

    Create About.js

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from'./About.module.css';
+
+const About = () => (
+    <div className={styles.about}>
+        About
+    </div>
+);
+
+export default About; 
